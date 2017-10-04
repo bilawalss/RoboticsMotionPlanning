1066843da44ac5d5da04401e3b77f251be41b50a
Thang Le <thangminh.le208@gmail.com>
2017-10-03 21:42:26 -0400

    separate sampler from environment

--- src/main/java/Environment.java
+++ src/main/java/Environment.java
@@ -8,16 +8,15 @@ import org.ejml.simple.SimpleMatrix;
 public class Environment {
 	private Robot robot;
 	private List<PolygonObject> obstacles;
-    Sampler sampler = new Sampler(600, 400);
 
 	public Environment(Robot robot, List<PolygonObject> obstacles) {
 		this.robot = robot;
 		this.obstacles = obstacles;
 	}
 
-    public static boolean pairCollision (PolygonObject p1, PolygonObject p2, Configuration c) {
-        BoundingBox b1 = p1.getBoundingBox();
-        BoundingBox b2 = p2.getBoundingBox();
+    private boolean pairCollision (PolygonObject obstacle, Configuration c) {
+        BoundingBox b1 = obstacle.getBoundingBox();
+        BoundingBox b2 = robot.getBoundingBox();
 
         // one on the left of the other
         if (b1.getMinX() > b2.getMaxX() + c.getX() || b2.getMinX() + c.getX() > b1.getMaxX())
@@ -30,21 +29,12 @@ public class Environment {
         return true;
     }
 
-    public Configuration getCollisionFreePoint() {
-        boolean done = true;
-        Configuration c = sampler.getSamplePoint();
-        while (done) { 
-            c = sampler.getSamplePoint();
-            int obstaclesCount = 0;
-            for (PolygonObject obstacle : obstacles) {
-                obstaclesCount++;
-                if(pairCollision(obstacle, robot, c )) {
-                    break;
-                }
+    public boolean checkCollision (Configuration c) {
+        for (PolygonObject o: obstacles) {
+            if (pairCollision(obstacle, c)) {
+                return true;
             }
-
-            if (obstaclesCount == obstacles.size()) done = false;
         }
-        return c;
+        return false;
     }
 }
