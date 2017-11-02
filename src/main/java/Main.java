8557f0cf4a1c5a59d6bcb7723366e6e2c8aea6f8
bilawalss <bilawal.shaikh@richmond.edu>
2017-11-01 23:01:18 -0400

    start work on animation, make PRM in its own class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -27,6 +27,12 @@ import planning.Environment;
 import planning.MotionPlanner;
 import planning.LocalPlanner;
 import planning.RRT;
+import planning.PRM;
+
+import javafx.animation.AnimationTimer;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 
 import static global.Constants.DEBUG;
 
@@ -36,6 +42,8 @@ import static global.Constants.DEBUG;
  * @author: Bilawal Shaikh and Thang Le
  */
 public class Main extends Application {
+
+    private AnimationTimer timer;
     
     private static void printObjectArray(Object[] data) {
         for (int i = 0; i < data.length; i++) {
@@ -128,7 +136,7 @@ public class Main extends Application {
         }
 
 
-        if (DEBUG) {
+        if (!DEBUG) {
             Group debugRoot = new Group();
             Stage debugStage = new Stage();
             Scene debugScene = new Scene(debugRoot, 600, 400);
@@ -189,118 +197,63 @@ public class Main extends Application {
             Environment env = new Environment(r, obstacles, 600, 400);
             Sampler sampler = new Sampler();
 
-            int size = 0;
-            Graph g = new Graph();
-            for (int i = 0; i < 10; i++) {
-                Configuration c = sampler.getSamplePoint(600, 400);
-                if (!env.checkCollision(c)) {
-                    size++;
-                    g.addVertex(c);
-                    Circle circle = new Circle();
-                    circle.setCenterX(c.getX());
-                    circle.setCenterY(c.getY());
-                    circle.setRadius(10.0);
-                    circle.setFill(Color.BLUE);
-                    prmRoot.getChildren().add(circle);
-                }
-            }
-             
+            
             LocalPlanner localPlanner = new LocalPlanner();
 
-            // initialize configurations
-            for (int i = 0; i < size; i++) {
-                // Closest vertices to the 5 closest ones.
-                List<Integer> neighbors = g.getKClosestVertices(i, 5);
+            Configuration start = new Configuration(200, 120, 0.3);
+            Configuration end = new Configuration(150, 350, 1.2);
 
-                // if the local planner does not have any obtacles between the object, add the
-                // object to the graph
-                for (int neighbor : neighbors) {
-                    if (localPlanner.getPath(env, g.getVertex(i), g.getVertex(neighbor), 10) != null) {
-                        g.addEdge(i, neighbor);
-                        Line line = new Line();
-                        line.setStartX(g.getVertex(i).getX());
-                        line.setStartY(g.getVertex(i).getY());
-                        line.setEndX(g.getVertex(neighbor).getX());
-                        line.setEndY(g.getVertex(neighbor).getY());
+            PRM prm = new PRM(env, sampler, localPlanner);
 
-                        line.setStroke(Color.BLUE);
-                        prmRoot.getChildren().add(line);
-                        
+            // third parameter is the number of sample iterations
+            Graph g = prm.buildRoadMap(600, 400, 100);       
+            drawGraph(prmRoot, g, Color.RED); 
+                
+            List<Configuration> points = prm.pathPlanning(g, start, end);    
 
-                    }
+            Timeline timeline = new Timeline();
+
+            timeline.setCycleCount(Timeline.INDEFINITE);
+            timeline.setAutoReverse(true);
+
+            //You can add a specific action when each frame is started.
+            /*
+            timer = new AnimationTimer() {
+                private long lastUpdate = 0;
+                int i = 0;
+                int j = 0;
+                @Override
+                public void handle(long now) {
+                    //drawGraph(prmRoot, g, Color.RED);
+                    Polygon poly = new Polygon();
+                    Configuration c = points.get(i);
+                    Configuration d = points.get(i+1);
+                    List<Configuration> secondaryPoints = localPlanner.getPath(env, c, d, 10.0);
+
+                    Configuration e = secondaryPoints.get(j);
+                    poly.getPoints().addAll(env.getRobotPointArray(e));
+                    prmRoot.getChildren().add(poly);
+
+                    //prmRoot.getChildren().remove(poly);
+                    //if (now - lastUpdate >= 28) {
+                        j++;
+                        if (e.equals(d)) {
+                            i++;
+                            j = 0;
+                        }
+                      //  lastUpdate = now;
+                   // }
                 }
-            }    
+            };
+            */
+            
+            //timer.start();
+            //timeline.play();
 
-            // start and end goal configuration
-
-             Configuration start = new Configuration(200, 120, 0.3);
-             Configuration end = new Configuration(150, 350, 1.2);
-
-            if (env.checkCollision(start)) return;
-            if (env.checkCollision(end)) return;
-
-
-            Circle circle = new Circle();
-            circle.setCenterX(start.getX());
-            circle.setCenterY(start.getY());
-            circle.setRadius(10.0);
-            circle.setFill(Color.RED);
-            prmRoot.getChildren().add(circle);
-
-            Circle circle2 = new Circle();
-            circle2.setCenterX(end.getX());
-            circle2.setCenterY(end.getY());
-            circle2.setRadius(10.0);
-            circle2.setFill(Color.PURPLE);
-            prmRoot.getChildren().add(circle2);
-
-             // add this to the graph
-
-             int startPos = g.addVertex(start);
-             int endPos = g.addVertex(end);
-        
-            for (int i = 0; i < 2; i++) {
-                int pos = 0;
-                if (i == 0) pos = startPos;
-                if (i == 1) pos = endPos;
-                 List<Integer> neighbors = g.getKClosestVertices(pos, 10);
-                 for (int neighbor : neighbors) {
-                    if (localPlanner.getPath(env, g.getVertex(pos), g.getVertex(neighbor), 10) != null) {
-                        g.addEdge(pos, neighbor);
-                        Line line = new Line();
-                        line.setStartX(g.getVertex(pos).getX());
-                        line.setStartY(g.getVertex(pos).getY());
-                        line.setEndX(g.getVertex(neighbor).getX());
-                        line.setEndY(g.getVertex(neighbor).getY());
-
-                        line.setStroke(Color.RED);    
-                        prmRoot.getChildren().add(line);
-                        
-
-                    }
-                }
-            }        
-
-            List<Integer> shortestPath = g.shortestPath(startPos, endPos);
-
-            for (int i = 0; i < shortestPath.size() - 1; i++) {
-                Configuration u = g.getVertex(shortestPath.get(i));
-                Configuration v = g.getVertex(shortestPath.get(i+1));
-
-                Line line = new Line();
-                line.setStartX(u.getX());
-                line.setStartY(u.getY());
-                line.setEndX(v.getX());
-                line.setEndY(v.getY());
-                line.setStroke(Color.YELLOW);
-                prmRoot.getChildren().add(line);
-            }
-    
-             prmRoot.getChildren().add(original);
-             prmStage.setScene(prmScene);
-             prmStage.show();
+            prmStage.setScene(prmScene);
+            prmStage.show();
   
-              return;
+            return;
         }
 
         if (DEBUG) {
---
+++ src/main/java/planning/PRM.java
@@ -0,0 +1,104 @@
+package planning;
+
+import geometry.Graph;
+import geometry.Configuration;
+
+import java.util.List;
+import java.util.ArrayList;
+
+
+public class PRM extends MotionPlanner {
+
+    public PRM(Environment env, Sampler sampler, LocalPlanner localPlanner) {
+        this.env = env;
+        this.sampler = sampler;
+        this.localPlanner = localPlanner;
+    }
+
+    public Graph buildRoadMap(int worldWidth, int worldHeight, int samplePoints) {
+    	long startTime = System.nanoTime();
+		
+
+    	int size = 0;
+        Graph g = new Graph();
+        for (int i = 0; i < samplePoints; i++) {
+            Configuration c = sampler.getSamplePoint(worldWidth, worldHeight);
+            if (!env.checkCollision(c)) {
+                size++;
+                g.addVertex(c);
+     
+            }
+            
+        }
+
+        long endTime = System.nanoTime();
+        long duration = (endTime - startTime);
+        System.out.println("Time to Sample points: "+ duration/1000000);
+
+        startTime = System.nanoTime();
+        // initialize configurations
+        for (int i = 0; i < size; i++) {
+            // Closest vertices to the 5 closest ones.
+            List<Integer> neighbors = g.getKClosestVertices(i, 5);
+
+            // if the local planner does not have any obtacles between the object, add the
+            // object to the graph
+            for (int neighbor : neighbors) {
+                if (localPlanner.getPath(env, g.getVertex(i), g.getVertex(neighbor), 10) != null) {
+                    g.addEdge(i, neighbor);
+                    
+                }
+            }
+        }  
+
+        endTime = System.nanoTime();  
+        duration = (endTime - startTime);
+        System.out.println("Time to use Local Planner to finish RoadMap: "+ duration/1000000);
+
+        return g;
+    }
+
+    public List<Configuration> pathPlanning(Graph g, Configuration start, Configuration end) {
+
+        if (env.checkCollision(start)) return null;
+        if (env.checkCollision(end)) return null;
+
+         // add this to the graph
+        long startTime = System.nanoTime();
+         int startPos = g.addVertex(start);
+         int endPos = g.addVertex(end);
+    
+        for (int i = 0; i < 2; i++) {
+            int pos = 0;
+            if (i == 0) pos = startPos;
+            if (i == 1) pos = endPos;
+             List<Integer> neighbors = g.getKClosestVertices(pos, 10);
+             for (int neighbor : neighbors) {
+                if (localPlanner.getPath(env, g.getVertex(pos), g.getVertex(neighbor), 10) != null) {
+                    g.addEdge(pos, neighbor);
+                    
+                }
+            }
+        }   
+
+        long endTime = System.nanoTime();
+        long duration = endTime - startTime;
+        System.out.println("Time to add Start and End Nodes to the RoadMap "+ duration/1000000);     
+
+
+        startTime = System.nanoTime();
+        List<Integer> shortestPath = g.shortestPath(startPos, endPos);
+        List<Configuration> res = new ArrayList<>();
+
+        endTime = System.nanoTime();
+        duration = endTime - startTime;
+        System.out.println("Time to find shortest path between start and end positions: "+ duration/1000000);
+
+        for (int i = 0; i < shortestPath.size(); i++) {
+            Configuration u = g.getVertex(shortestPath.get(i));
+        	res.add(u);
+        }  
+
+        return res;
+    }    
+}
